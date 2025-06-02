4777c20228a71dccc85a8bfad7df95c211e490bb
Pranjal Snehi <pranjal.snehi@gmail.com>
2025-06-02 19:00:34 +0530

    Routes and Models defined

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-import React from 'react'
+const express = require('express')
+const app = express();
+const path = require('path')
+const fs = require('fs')
+// const userModel = require('./models/user-model')
+const db = require('./config/mongoose-connection');
 
-function app() {
-  return (
-    <div>App</div>
-  )
-}
+const ownersRouter = require('./routes/ownersRouter')
+const productsRouter = require('./routes/productsRouter')
+const usersRouter = require('./routes/usersRouter')
+//Parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
 
-export default app
+app.use("/owners", ownersRouter)
+app.use("/users", usersRouter)
+app.use("/products", productsRouter)
+
+
+app.listen(3000);
